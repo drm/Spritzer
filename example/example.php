9917eb8952801367a7cd87843a44c8ba8030106d
drm <drm@melp.nl>
2011-01-23 14:45:40 +0100

    restructured example

--- example/example.php
+++ example/example.php
@@ -8,7 +8,5 @@
 require_once dirname(__FILE__) . '/../lib/Spritzer/autoload.php';
 $parser = new Spritzer_Parser('example.sprite');
 $config = $parser->parse();
-$config->image()->writeTo('./example.png');
-$config->css('./example.png')->writeTo('./example.css');
-
-
+$config->image()->writeTo('./images/sprite.png');
+$config->css('../images/sprite.png')->writeTo('css/example.css');
