76b2338ad0e2d3ea730b78196db4b99e88286cce
drm <drm@melp.nl>
2011-01-23 16:29:52 +0100

    Made template configurable

--- lib/Spritzer/Css.php
+++ lib/Spritzer/Css.php
@@ -5,6 +5,18 @@
 
 class Spritzer_Css implements Spritzer_Css_Interface
 {
+    const SHORTHAND_TEMPLATE = '%s { background: url(%s) %s %s no-repeat; }';
+    const EXPANDED_TEMPLATE = '%s { background-image: url(%s); background-position: %s %s; background-repeat: no-repeat; }';
+
+    protected $selectors = array();
+    protected $imageUrl = null;
+    protected $template = null;
+
+    function __construct($template = self::SHORTHAND_TEMPLATE) {
+        $this->template = $template;
+    }
+
+
     function setSelector($selector, $left, $top)
     {
         $this->selectors[$selector] = array($left, $top);
@@ -28,8 +40,23 @@ class Spritzer_Css implements Spritzer_Css_Interface
     {
         $css = '';
         foreach ($this->selectors as $selector => $position) {
-            $css .= sprintf('%s { background: url(%s) %dpx %dpx no-repeat; }', $selector, $this->imageUrl, $position[1], $position[0]) . "\n";
+            $css .= sprintf(
+                $this->template,
+                $selector,
+                $this->imageUrl,
+                $this->_unit($position[1]),
+                $this->_unit($position[0])
+            ) . "\n";
         }
         return $css;
+    }
+
+
+
+    private function _unit($s) {
+        if($s == 0) {
+            return $s;
+        }
+        return $s . 'px';
     }
 }
