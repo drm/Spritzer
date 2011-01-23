d5ca94ea2b964eb2a2cc292566b7b7e1a25120e0
drm <drm@melp.nl>
2011-01-23 14:39:37 +0100

    adaptive resize if source image is larger than available space

--- lib/Spritzer/Sprite.php
+++ lib/Spritzer/Sprite.php
@@ -45,20 +45,31 @@ class Spritzer_Sprite implements Spritzer_Sprite_Interface
     function setTile($x, $y, $value)
     {
         if (!$this->_imageResource) {
-            $this->_createImg();
-
+            $this->_createImage($this->tableWidth * $this->tileWidth, $this->tableHeight * $this->tileHeight);
         }
         $this->pasteAt($this->tileWidth * $x, $this->tileHeight * $y, $value);
     }
 
 
-    private function _createImg()
+    private function _createImage($width, $height)
     {
-        $this->_imageResource = imagecreatetruecolor($this->tableWidth * $this->tileWidth, $this->tableHeight * $this->tileHeight);
+        $this->_imageResource = imagecreatetruecolor($width, $height);
+
         imagealphablending($this->_imageResource, false);
         imagefilledrectangle($this->_imageResource, 0, 0, imagesx($this->_imageResource), imagesy($this->_imageResource), imagecolorallocatealpha($this->_imageResource, 255, 255, 255, 127));
         imagealphablending($this->_imageResource, true);
         imagesavealpha($this->_imageResource, true);
+    }
+
+
+    private function _expand($newWidth, $newHeight) {
+        $newWidth = max(imagesx($this->_imageResource), $newWidth);
+        $newHeight = max(imagesy($this->_imageResource), $newHeight);
+
+        $oldImg = $this->_imageResource;
+        $this->_createImage($newWidth, $newHeight);
+        imagecopy($this->_imageResource, $oldImg, 0, 0, 0, 0, imagesx($oldImg), imagesy($oldImg));
+        imagedestroy($oldImg);
     }
 
 
@@ -83,6 +94,13 @@ class Spritzer_Sprite implements Spritzer_Sprite_Interface
         }
 
         $im = $callback($fileName);
+        
+        if(
+            $x + imagesx($im) > imagesx($this->_imageResource)
+        ||  $y + imagesy($im) > imagesy($this->_imageResource)
+        ) {
+            $this->_expand($x + imagesx($im), $y + imagesy($im));
+        }
         imagecopy($this->_imageResource, $im, $x, $y, 0, 0, imagesx($im), imagesy($im));
         imagedestroy($im);
     }
